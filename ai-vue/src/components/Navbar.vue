b0a3b63f96e4c35b7b39136c574712bd1ae33a29
ZhouGuo298 <2795355316@qq.com>
2026-05-27 00:48:23 +0800

    新增主页与注册页

--- ai-vue/src/components/Navbar.vue
+++ ai-vue/src/components/Navbar.vue
@@ -9,7 +9,7 @@
         <div class="flex-box">
             <el-dropdown @command="handleCommand">
                 <div class="flex-box">
-                    <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
+                    <el-avatar :src="logoUrl" />
                     <p class="user-name">admin</p>
                     <el-icon><ArrowDown /></el-icon>
                 </div>
@@ -27,7 +27,7 @@
 
 <script setup>
 import { useAdminStore } from '@/stores/admin'
-import { ref } from 'vue'
+import logoUrl from '@/assets/logo.png'
 
 const handleCommand = (command) => {
     console.log(command)
